b7c2af0cfd7673222283a4e38269abe4e81c1114
Prithvi <prithvimanojoffc18@gmail.com>
2024-03-28 13:31:04 +0530

    field push

---
+++ Field/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './Components/Header';
+import { NavigationContainer } from '@react-navigation/native';
+import Form from './Components/Form';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <View style={styles.container}>
+      <Header />
+      <StatusBar style="auto" />
+      <Form />
+    </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    
+  },
+  statusbar:{
+    backgroundColor:"red"
+  }
+});
